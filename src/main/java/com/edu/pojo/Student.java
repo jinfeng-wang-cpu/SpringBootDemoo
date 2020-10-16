8eac24941f23e0b047788a6e68161b18b1c63e97
KongBai0206 <65081862+KongBai0206@users.noreply.github.com>
2020-10-16 12:53:26 +0800

    Hello SpringBoot!

---
+++ src/main/java/com/edu/pojo/Student.java
@@ -0,0 +1,50 @@
+package com.edu.pojo;
+
+public class Student {
+
+    Integer stuId;
+    String stuName;
+    Integer age;
+    String sex;
+    String address;
+
+    public Integer getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(Integer stuId) {
+        this.stuId = stuId;
+    }
+
+    public String getStuName() {
+        return stuName;
+    }
+
+    public void setStuName(String stuName) {
+        this.stuName = stuName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
